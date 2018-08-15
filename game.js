c3a174b86c0410e8baa490db42683213f7443087
Richa Sharma <richasharma@Richas-MacBook-Air.local>
2018-08-15 13:37:08 +0530

    Added files for the game

---
+++ game.js
@@ -0,0 +1,43 @@
+
+let sign = "X";
+let disp = document.getElementById("player");
+
+function printx(number) {
+    let isko = document.getElementById("r" + number);
+    console.log(isko);
+
+    if(isko.innerText==""){ 
+    isko.innerText = sign;
+    
+    checksign();
+    disp.innerHTML= "<center>" + sign + " Turn " + "</center>" ;
+    winner();
+    }
+}
+
+
+function checksign() {
+     if (sign == "X") sign = "O";
+     else sign = "X";
+}
+
+function getbox(no) {
+	return document.getElementById("r" + no).innerHTML;
+}
+
+function checkmove(a,b,c,m) {
+	if(getbox(a)== m && getbox(b)== m && getbox(c)== m)
+	return true;
+	else return false;
+}
+
+function winner() {
+if(checkmove(1,2,3,"X") || checkmove(4,5,6,"X") || checkmove(7,8,9,"X") ||
+(checkmove(1,4,7,"X") || checkmove(2,5,8,"X") || checkmove(3,6,9,"X") ||
+(checkmove(1,5,9,"X") || checkmove(7,5,3,"X")) 
+disp.innerHTML = "<center>" + sign + " Wins " + "</center>";
+for(let i = 1; i <= 9; i++){
+   document.getElementById("r" + i).iinerHTML ="";
+}   
+}
+}
